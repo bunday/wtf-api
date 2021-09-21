e5f652d48761fd3f463d7e83980230bce437458b
Zadat Olayinka <bundayy@outlook.com>
2021-09-21 11:42:10 +0100

    user model + token creation

---
+++ api/auth.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const router = express.Router();
+const UserService = require("../service/userService");
+const sendErrorResponse =
+  require("../service/responseService").sendErrorResponse;
+const sendSuccessResponse =
+  require("../service/responseService").sendSuccessResponse;
+
+router.post("/register", async function (req, res) {
+  try {
+    const data = req.body;
+
+    if (!data.username) {
+      return sendErrorResponse(req, res, {
+        code: 400,
+        message: "Username is required.",
+      });
+    }
+
+    if (!data.password) {
+      return sendErrorResponse(req, res, {
+        code: 400,
+        message: "Password is required.",
+      });
+    }
+
+    if (!data.confirm_password) {
+      return sendErrorResponse(req, res, {
+        code: 400,
+        message: "Confirm Password is required.",
+      });
+    }
+
+    if (data.password != data.confirm_password) {
+      return sendErrorResponse(req, res, {
+        code: 400,
+        message: "Password does not match.",
+      });
+    }
+
+    // confirm that the username doesnt exist already
+    const existingUser = await UserService.findOneBy({
+      username: data.username,
+    });
+    if (existingUser) {
+      return sendErrorResponse(req, res, {
+        code: 400,
+        message: "Username is taken.",
+      });
+    }
+
+
+    // all checks are fine, now we save to the db
+    const user = await UserService.create(data);
+    // generate token for user
+    const userDto = {token: UserService.generateAccessToken(user.username)}
+
+    return sendSuccessResponse(
+      req,
+      res,
+      userDto,
+      "User Created Successfully"
+    );
+  } catch (error) {
+    return sendErrorResponse(req, res, error);
+  }
+});
+
+
+module.exports = router;
---
+++ service/userService.js
@@ -0,0 +1,34 @@
+module.exports = {
+  create: async function (data) {
+    try {
+      const userModel = new UserModel();
+      userModel.username = data.username;
+      userModel.password = await bcrypt.hash(data.password, 10);
+
+      const user = await userModel.save();
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  },
+  async findOneBy(query) {
+    try {
+      if (!query) {
+        query = {};
+      }
+
+      if (!query.deleted) query.deleted = false;
+      const user = await UserModel.findOne(query);
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  },
+  generateAccessToken(username) {
+    return jwt.sign(username, process.env.TOKEN_SECRET);
+  },
+};
+
+const UserModel = require("../model/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
